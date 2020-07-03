684e0b90a2f90c01fc3bdad3f975d5665b59fbc1
Abel Pacheco <abelpacheco@outlook.com>
2020-07-03 09:20:44 -0500

    Add Project for Mac OS X

---
+++ src Mac OS X/mec4bars/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mec4bars
+//
+//  Created by Abel Pacheco on 03/07/20.
+//  Copyright © 2020 Abel Pacheco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
